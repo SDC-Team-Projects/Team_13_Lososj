88f7b326f449704fdf1cb901f4e9c2c2a10eb1c6
lana <ialanta.pushchayenka@student.ehu.lt>
2026-04-29 19:07:40 +0300

    add vite project files

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react'
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage/>}></Route>
+      <Route path="/register" element={<RegisterPage/>} ></Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
