e1bd113681d05f777a27e046bef6ed60b398e22c
lana <ialanta.pushchayenka@student.ehu.lt>
2026-04-30 08:34:35 +0300

    edit  home page

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage/>}></Route>
-      <Route path="/" element={<RegisterPage/>} />
+      {/* <Route path="/" element={<RegisterPage/>} /> */}
+      <Route path="/" element={<h1>HOME TEST</h1>} />
     </Routes>
     </BrowserRouter>
     </>
