32e27932264fedc79d12093896b2775f37aac0ea
Kirill Verhavodka <88534242+Bruh-2@users.noreply.github.com>
2026-05-09 13:55:20 +0300

    Update app.js

--- collections-api/src/app.js
+++ collections-api/src/app.js
@@ -57,7 +57,7 @@ app.post("/api/auth/register", async (req, res) => {
     }
 
     //  проверка длины пароля
-    if (password.length < 6) {
+    if (password.length < 8) {
       return res.status(400).json({
         error: "Password must be at least 6 characters",
       });
