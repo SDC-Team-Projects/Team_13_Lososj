b10b6c89f257c3a2312da8629bde1f20dcdea9bf
DP-63-A <129793050+DP-63-A@users.noreply.github.com>
2026-05-23 23:32:30 +0300

    Update index.js

--- collections-api/src/db/index.js
+++ collections-api/src/db/index.js
@@ -2,7 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "test" ? false : { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
