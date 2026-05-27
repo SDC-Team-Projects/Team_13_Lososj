7bcf511e9c3b1f0fcb11d919cf9f1c885ff8a079
DP-63-A <129793050+DP-63-A@users.noreply.github.com>
2026-05-27 22:34:54 +0300

    Update app.js

--- collections-api/src/app.js
+++ collections-api/src/app.js
@@ -1568,6 +1568,10 @@ app.get("/api/analytics/collection/:id", auth, async (req, res) => {
 /* USER ANALYTICS */
 app.get("/api/analytics/user", auth, async (req, res) => {
   try {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+
     const userId = req.user.id;
 
     const collections = await pool.query(
