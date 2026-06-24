5c1fedda6c0ebbdbd054e4bb07a2104d2a53a1e4
Kirill Verhavodka <88534242+Bruh-2@users.noreply.github.com>
2026-06-24 14:16:40 +0300

    Replace nodemailer with Resend for email sending

--- collections-api/src/mailer.js
+++ collections-api/src/mailer.js
@@ -1,11 +1,30 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-module.exports = transporter;
+async function sendResetEmail(email, link) {
+
+  await resend.emails.send({
+
+    from: "onboarding@resend.dev",
+
+    to: email,
+
+    subject: "Reset your password",
+
+    html: `
+      <h2>Password reset</h2>
+
+      <p>Click the link below to reset your password.</p>
+
+      <a href="${link}">
+        Reset password
+      </a>
+    `
+  });
+
+}
+
+module.exports = {
+  sendResetEmail
+};
